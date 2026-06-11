8f5f92f450c2100d8912a1230affab4da00bdd60
Maimoon <maimoon.ideal@gmail.com>
2026-06-11 11:11:45 +0500

    feat: register orchestration signals in app configuration

--- orchestration/apps.py
+++ orchestration/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class OrchestrationConfig(AppConfig):
+    """Django app config for the orchestration multi-agent pipeline."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orchestration'
+
+    def ready(self):
+        """Import signals when the app is ready."""
+        import orchestration.signals  # noqa: F401
